b9e96f096e3b513801908518dfa53bfc1a90e36b
SarmaBharadwaj <chickkubharadwajsarma@gmail.com>
2025-11-23 13:02:41 +0530

    fix: add CORS middleware to allow Static Web App origin

--- Functions/Program.cs
+++ Functions/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Middleware;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Quizz.DataAccess;
 using Quizz.Auth;
 using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Functions.Worker.Http;
 
 namespace Quizz.Functions
 {
@@ -13,7 +16,11 @@ namespace Quizz.Functions
         public static void Main()
         {
             var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults()
+                .ConfigureFunctionsWorkerDefaults(app =>
+                {
+                    // Add CORS middleware
+                    app.UseMiddleware<CorsMiddleware>();
+                })
                 .ConfigureServices((context, services) =>
                 {
                     // Get configuration
@@ -42,6 +49,37 @@ namespace Quizz.Functions
                 .Build();
 
             host.Run();
+        }
+    }
+
+    // CORS Middleware for Azure Functions
+    public class CorsMiddleware : IFunctionsWorkerMiddleware
+    {
+        public async Task Invoke(FunctionContext context, FunctionExecutionDelegate next)
+        {
+            var requestData = await context.GetHttpRequestDataAsync();
+            
+            if (requestData != null)
+            {
+                // Get the response
+                await next(context);
+
+                // Add CORS headers to response
+                var response = context.GetHttpResponseData();
+                if (response != null)
+                {
+                    // Allow your Static Web App domain
+                    response.Headers.Add("Access-Control-Allow-Origin", "https://black-bush-0d3fb621e.3.azurestaticapps.net");
+                    response.Headers.Add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH");
+                    response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, Authorization, X-API-Key");
+                    response.Headers.Add("Access-Control-Allow-Credentials", "true");
+                    response.Headers.Add("Access-Control-Max-Age", "86400");
+                }
+            }
+            else
+            {
+                await next(context);
+            }
         }
     }
 }
